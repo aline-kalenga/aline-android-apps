dd730bb4df3f5ba8f500013c565a502036448d86
Tim-lubo <timlubo@softcongoastral.com>
2020-06-03 08:50:59 +0200

    assignement 1

---
+++ AlineAndroidTpApp/app/src/main/java/com/example/alinetpone/PeopleActivity.java
@@ -0,0 +1,14 @@
+package com.example.alinetpone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PeopleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.people_layout);
+    }
+}
